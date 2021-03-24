b5f9b21c24c29a5745a2ad0c574f34a7ede2fbb9
Lennox Stark <stark47@Lennoxs-iMac.local>
2021-03-24 23:59:40 +0530

    added styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Container } from 'react-bootstrap';
 import Signup from "./Signup.component"
 
 const App = () => {
   return (
-    <Signup />
+    <Container className="d-flex align-items-center justify-content-center" 
+    style={{minHeight:'70vh'}}
+    >
+      <div className="w-100" style={{maxWidth : '400px'}}>
+        <Signup />
+      </div>
+
+    </Container>
+
   )
 }
 
